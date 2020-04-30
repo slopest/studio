65f4671d8754f72f4944ff5c157155fa1ae7ceeb
Théo Vidal <exybore@becauseofprog.fr>
2020-04-30 22:09:01 +0200

    wip: add first 3D representation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Highest Logo"
-          class="shrink mr-2"
-          contain
-          src="/img/logo-large.png"
-          width="150"/>
-      </div>
-
-      <v-spacer/>
-
-      <v-btn
-        href="https://github.com/highest-app/highest/releases/latest"
-        target="_blank"
-        text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Modest Studio</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,138 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          src="/img/logo.png"
-          class="my-3"
-          transition="scale-transition"
-          height="200"
-          contain/>
+    <v-row>
+      <v-col cols="6">
+        <h2>Editor</h2>
+        <v-row>
+          <v-col cols="12">
+            <v-slider
+              v-model="width"
+              min="1"
+              max="25"
+              label="Width"
+              prepend-icon="mdi-arrow-left-right"
+              thumb-label/>
+            <v-slider
+              v-model="height"
+              min="2"
+              max="55"
+              label="Height"
+              prepend-icon="mdi-arrow-up-down"
+              thumb-label/>
+          </v-col>
+          <v-col cols="12">
+            <v-row
+              v-for="h in height"
+              :key="h">
+              <v-btn
+                v-for="w in width"
+                :key="`${h}-${w}-btn`"
+                icon>
+                <v-icon>mdi-circle</v-icon>
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-row>
       </v-col>
+      <div style="max-width: 50%">
+        <h2>Preview</h2>
+        <div>
+          <div style="display: flex">
+            <vgl-renderer
+              ref="renderer"
+              antialias>
+              <vgl-scene ref="scene">
+                <vgl-mesh-standard-material
+                  name="screw-material"
+                  color="#B2BABB"/>
+                <vgl-mesh-standard-material
+                  name="wall-material"
+                  color="#EB984E"/>
+                <!--<vgl-obj-loader
+                  position="60 60 -10"
+                  name="hold"
+                  src="/holds/sloper.obj"/>
+                <vgl-mesh
+                  geometry="hold"
+                  material="mat"/>-->
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Highest Template
-        </h1>
+                <vgl-plane-geometry
+                  name="wall"
+                  :width="width * 20"
+                  :height="height * 20"/>
+                <vgl-mesh
+                  :position="`${10 + (width * 20 / 2)} ${10 + (height * 20 / 2)} 0`"
+                  geometry="wall"
+                  material="wall-material"/>
 
-        <p class="subheading font-weight-regular">
-          This is a front-end template including : Vue.js, Vuetify, Router, PWA
-        </p>
-      </v-col>
+                <template v-for="h in height">
+                  <template v-for="w in width">
+                    <vgl-sphere-geometry
+                      :key="`${h}-${w}-model`"
+                      :name="`hold-${h}-${w}`"
+                      radius="2"
+                      width-segments="64"
+                      height-segments="32"/>
+                    <vgl-mesh
+                      :key="`${h}-${w}-mesh`"
+                      :geometry="`hold-${h}-${w}`"
+                      :position="`${w * 20} ${h * 20} 0`"
+                      material="screw-material"/>
+                  </template>
+                </template>
+                <vgl-ambient-light intensity="0.5"/>
+                <vgl-directional-light
+                  position="1 1 1"
+                  intensity="0.5"/>
+              </vgl-scene>
+              <vgl-perspective-camera
+                ref="camera"
+                name="camera"
+                orbit-position="200 1.5 0"/>
+            </vgl-renderer>
+          </div>
+        </div>
+      </div>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { VglRenderer, VglScene, VglPlaneGeometry, VglMeshStandardMaterial, VglPerspectiveCamera, VglMesh, VglSphereGeometry, VglAmbientLight, VglDirectionalLight } from 'vue-gl'
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 export default {
   name: 'Home',
+  components: { VglSphereGeometry, VglPlaneGeometry, VglRenderer, VglScene, VglMeshStandardMaterial, VglPerspectiveCamera, VglMesh, VglAmbientLight, VglDirectionalLight},
   data () {
     return {
-
+      height: 11,
+      width: 5,
+      controls: undefined
+    }
+  },
+  mounted() {
+    this.controls = new OrbitControls(this.$refs.camera.inst, this.$refs.renderer.inst.domElement)
+    this.controls.enableDamping = true
+    this.animate()
+    console.warn(this.$refs)
+  },
+  methods: {
+    animate() {
+      requestAnimationFrame(this.animate)
+      this.controls.update()
+      this.render()
+    },
+    render() {
+      this.$refs.renderer.inst.render(this.$refs.scene.inst, this.$refs.camera.inst)
     }
   }
 }
 </script>
+
+<style>
+  canvas {
+    max-height: 80vh
+  }
+</style>
